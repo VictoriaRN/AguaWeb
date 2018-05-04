ba38c60ecad5caaafd81f5c263fcac6a929f22e6
VictoriaRN <victoria.reynoso@alumnos.udg.mx>
2018-05-04 18:21:56 -0500

    Codicioso practica
    (Re-subido) Tuve un problema con un using en los archivos originales, (por eso la modificación de otro proyecto), no se si era de esta manera el programa, pero este es el resultado final y funcional

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -4,9 +4,9 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using Agua.Models;
+using codicioso.Models;
 
-namespace Agua.Controllers
+namespace codicioso.Controllers
 {
     public class HomeController : Controller
     {
@@ -16,15 +16,41 @@ namespace Agua.Controllers
         }
 
         
-        public IActionResult Calcular(DateTime ProximoCumple, int minutos)
+        public IActionResult NumMonedas(float DineroQueseDebe)
         {
-            /// Aquí tienen que hacer todo
-            /// La fecha que y los minutos vienen del formulario 
-            /// Investigar Model Binding 
+            int cambio_en_monedas,m5, m1,m50C,m10C,m1C;
+            float mod,val_entero,mod2,val_entero1,mod3,val_entero3,mod4,valentero4,val_entero5;
 
+          mod = DineroQueseDebe%5.0f; 
+          val_entero = DineroQueseDebe-mod;
+          int a = Convert.ToInt32(val_entero);
+          m5 = a/5;
 
-            ViewBag.Dias = 0;
-            ViewBag.Botellas = 0;
+          mod2 = mod%1.0f;
+          val_entero1= mod-mod2;
+          int b = Convert.ToInt32(val_entero1);
+          m1 = b/1;
+
+          mod3 = mod2%0.5f;
+          val_entero3 = mod2-mod3;
+          float c = val_entero3/0.5f;
+          m50C = Convert.ToInt32(c);
+
+          mod4 = mod3%0.1f;
+          valentero4 = mod3-mod4;
+          float d = valentero4/0.1f;
+          m10C = Convert.ToInt32(d);
+          val_entero5 = mod4/0.01f;
+          m1C = Convert.ToInt32(val_entero5);
+
+          cambio_en_monedas = m5 + m1 + m50C + m10C + m1C;
+
+            ViewBag.DineroEnMonedas = cambio_en_monedas;
+            ViewBag.Monedasde5 = m5;
+            ViewBag.Monedasde1 = m1;
+            ViewBag.Monedasde50c = m50C;
+            ViewBag.Monedasde10c = m10C;
+            ViewBag.Monedasde1c = m1C;
 
             return View();
         }
--- Program.cs
+++ Program.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
-namespace Agua
+namespace codicioso
 {
     public class Program
     {
